7cc50752609d140f198446340af4495979853679
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-27 00:39:38 +0000

    Deploy illak/salario_viz_rechart to github.com/illak/salario_viz_rechart.git:master

---
+++ precache-manifest.9061b0eea964e05231fa320acf8e22f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3909591a818c18e9f94",
+    "url": "/salario_viz_rechart/static/js/runtime~main.18c32bf0.js"
+  },
+  {
+    "revision": "8864388a5fa96dcc5bff",
+    "url": "/salario_viz_rechart/static/js/main.561061da.chunk.js"
+  },
+  {
+    "revision": "622c82eb4699a4bb05e7",
+    "url": "/salario_viz_rechart/static/js/2.c5ceb51c.chunk.js"
+  },
+  {
+    "revision": "8864388a5fa96dcc5bff",
+    "url": "/salario_viz_rechart/static/css/main.941eba1e.chunk.css"
+  },
+  {
+    "revision": "69af91073c9d69a4825c0bcc04d99397",
+    "url": "/salario_viz_rechart/index.html"
+  }
+];
--- src/components/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-	apiKey: 'AIzaSyD49U7s7Z5xZEHFgcO2tzLQel4oNBBFUZ4',
-	discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-	spreadsheetId: '1rFRJR08PnZETrorlMZrLDz0qN90sWAe3-mQj60KItJ8'
-};
