236cca41a94c9ac373bcb200e672ef51d78cc2cf
Illak Zapata <i.zapata1989@gmail.com>
2019-03-26 23:27:49 -0300

    fixex config file

--- src/components/config.js
+++ src/components/config.js
@@ -3,3 +3,5 @@ export default {
 	discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
 	spreadsheetId: '1rFRJR08PnZETrorlMZrLDz0qN90sWAe3-mQj60KItJ8'
 };
+
+console.log(process.env.APIKEY);
